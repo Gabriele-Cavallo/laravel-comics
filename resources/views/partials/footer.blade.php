eb80278fdf3d974a22558809c89376e2489873fc
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-05-20 16:04:26 +0200

    feat: add footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,89 @@
 <footer>
-    <h1>ciao sono il footer</h1>
+    <div class="container">
+        <div class="links-container">
+            <div class="ul-col">
+                <ul>
+                    <span>DC COMICS</span>
+                    <li>Character</li>
+                    <li>Comics</li>
+                    <li>Movies</li>
+                    <li>TV</li>
+                    <li>Games</li>
+                    <li>Videos</li>
+                    <li>News</li>
+                </ul>
+                <ul>
+                    <span>SHOP</span>
+                    <li>Shop DC</li>
+                    <li>Shop DC Collectibles</li>
+                </ul>
+            </div>
+            <div class="ul-col">
+                <ul>
+                    <span>DC</span>
+                    <li>Terms Of Use</li>
+                    <li>Privacy policy(New)</li>
+                    <li>Ad Choices</li>
+                    <li>Advertising</li>
+                    <li>Jobs</li>
+                    <li>Subscription</li>
+                    <li>Talent Workshops</li>
+                    <li>CPSC Certificates</li>
+                    <li>Ratings</li>
+                    <li>Shop Help</li>
+                    <li>Contact Us</li>
+                </ul>
+            </div>
+            <div class="ul-col">
+                <ul>
+                    <span>SITES</span>
+                    <li>DC</li>
+                    <li>MAD Magazine</li>
+                    <li>DC Kids</li>
+                    <li>DC Universe</li>
+                    <li>DC Power Visa</li>
+                </ul>
+            </div>
+        </div>
+        <div class="footer-logo">
+            <img src="{{ Vite::asset('resources/img/dc-logo-bg.png') }}" alt="logo">
+        </div>
+    </div>
+    <div class="social-footer">
+        <div class="container">
+            <button class="btn">
+                <a href="#">SIGN-UP NOW!</a>
+            </button>
+            <div class="social-wrapper">
+                <span>FOLLOW US</span>
+                <ul>
+                    <li>
+                        <a href="#">
+                            <img src="{{ Vite::asset('resources/img/footer-facebook.png') }}" alt="social link">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="{{ Vite::asset('resources/img/footer-twitter.png') }}" alt="social link">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt="social link">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="{{ Vite::asset('resources/img/footer-pinterest.png') }}" alt="social link">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="{{ Vite::asset('resources/img/footer-periscope.png') }}" alt="social link">
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </footer>
