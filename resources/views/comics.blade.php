9a3a3d825ad101129a02f7e7049c8f9bd2dc4e2b
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-05-20 16:14:26 +0200

    feat: add main button

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -1,8 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-    <section id="home">
+    <section id="main">
         <div class="container">
+            <div class="badge series">CURRENT SERIES</div>
             <ul>
                 @foreach ($comics as $comic)
                     <li>
@@ -11,6 +12,7 @@
                     </li>
                 @endforeach
             </ul>
+            <div class="badge load">LOAD MORE</div>
         </div>
     </section>
 @endsection
